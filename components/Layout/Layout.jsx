f209088927779b13ea48900f9fdd9525eb6140ac
bahstiien <bastien.lecalvez@gmail.com>
2022-02-08 14:06:31 +0100

    add meta

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -6,7 +6,13 @@ import Footer from "../Footer";
 export default function Layout({ children, pageTitle }) {
   return (
     <>
-      <Head />
+      <Head>
+        <title>Cécile Palavit, avocate à Lyon</title>
+        <meta
+          name="description"
+          content="Maitre Cécile Palavit est avocate en droit immobilier et en droit de la construction à Lyon"
+        />
+      </Head>
       <Navbar />
       <h1 className="font-second text-center text-5xl pt-32 "> {pageTitle}</h1>
       <main className="font-thrid pb-36 min-h-screen">{children}</main>

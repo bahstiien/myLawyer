f681e1d86023f2c1b5da4f52af981f7e6dd6a17a
bahstiien <bastien.lecalvez@gmail.com>
2022-04-11 18:29:29 +0200

    resolve some little pb

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Image from "next/image";
 import cpalavit from "../public/img/cpalavit.jpg";
 import style from "../styles/welcome.module.css";
-import SectionLayout from "./Layout/SectionLayout";
 import { LinkedinOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import styleBtn from "../styles/button.module.css";
@@ -24,7 +23,7 @@ const Welcome = () => {
           Avocat au barreau de Lyon
         </p>
         <div className="flex justify-around mt-8 flex-col md:flex-row">
-          <div className="w-full md:w-2/5">
+          <div className="w-full max-h-44 md:w-2/5">
             <p className={style.description}>
               Maître Cécile Palavit est avocat, inscrite au Barreau de Lyon.
               Elle exerce principalement en droit immobilier, droit public et
@@ -33,28 +32,52 @@ const Welcome = () => {
 
             <p className={style.description}>
               Me Palavit exerce, de manière complémentaire et dans le respect
-              strict de la prévention des conflits d’intérêt :
+              strict de la prévention des conflits d’intérêts :
             </p>
 
             <p className={style.description}>
-              - A titre principal, en qualité de collaboratrice libérale du
-              cabinet Adaltys (Lyon), au sein de l’équipe “droit immobilier” qui
-              accompagne ses clients sur les problématiques immobilières
-              diverses et montages complexes, en conseil et en contentieux ;
+              <li>
+                {" "}
+                A titre principal, en qualité de collaboratrice libérale du
+                <Link href="https://adaltys.com/" passHref>
+                  <a target="_blank"> cabinet Adaltys</a>
+                </Link>
+                (Lyon), au sein de l’équipe “droit immobilier” qui accompagne
+                ses clients sur les problématiques immobilières diverses et
+                montages complexes, en conseil et en contentieux{" "}
+              </li>
             </p>
             <p className={style.description}>
-              - En tant que consultante auprès du cabinet Lexinsight (Paris)
-              pour les problématiques immobilières de ce cabinet spécialisé en
-              conseil juridique et stratégique des entreprises ;
+              <li>
+                {" "}
+                En tant que consultante auprès du cabinet{" "}
+                <Link
+                  href="https://www.linkedin.com/company/lex-insight/?originalSubdomain=fr"
+                  passHref
+                >
+                  <a target="_blank">cabinet Lexinsight</a>
+                </Link>{" "}
+                (Paris) pour les problématiques immobilières de ce cabinet
+                spécialisé en conseil juridique et stratégique des entreprises{" "}
+              </li>
             </p>
 
             <p className={style.description}>
-              - A titre individuel, Me Palavit conseille et représente des
-              entreprises et des particuliers dans le cadre de leurs projets et
-              problématiques immobilières. Elle accompagne en particulier ses
-              clients sur des problématiques liées aux baux commerciaux, baux
-              d’habitation et contrats divers du secteur de la construction.
+              <li>
+                A titre individuel, Me Palavit conseille et représente des
+                entreprises et des particuliers dans le cadre de leurs projets
+                et problématiques immobilières. Elle accompagne en particulier
+                ses clients sur des problématiques liées aux baux commerciaux,
+                baux d’habitation et contrats divers du secteur de la
+                construction.{" "}
+              </li>
             </p>
+            <div className={showBio ? "hidden" : "flex justify-center"}>
+              <button className={styleBtn.button3} onClick={handleToggle}>
+                {" "}
+                En savoir plus
+              </button>
+            </div>
             <div className={showBio ? "" : "hidden"}>
               <p className="italic"> Formation </p>
               <p className={style.description}>
@@ -76,34 +99,30 @@ const Welcome = () => {
               </p>
             </div>
           </div>
+          <div className="flex flex-col justify-center">
+            <div className="hidden md:flex justify-end max-h-sm max-w-sm mx-24">
+              <Image
+                src={cpalavit}
+                objectFit="cover"
+                alt="photo of cecile palavit"
+                className={style.photo}
+              />
+            </div>
 
-          <div className="hidden md:flex justify-end max-h-sm max-w-sm mx-24">
-            <Image
-              src={cpalavit}
-              // layout={"fill"}
-              objectFit="cover"
-              alt="photo of cecile palavit"
-              className={style.photo}
-            />
+            {/* <Link
+              href="https://www.linkedin.com/in/c%C3%A9cile-palavit-5a765683/"
+              passHref
+            >
+              <a target="_blank">
+                <LinkedinOutlined
+                  style={{ fontSize: "48px", color: "#0077b5" }}
+                />
+              </a>
+            </Link> */}
           </div>
         </div>
-        <div className="flex justify-around mt-4">
-          <button className={styleBtn.button3} onClick={handleToggle}>
-            {" "}
-            En savoir plus
-          </button>
-          <Link
-            href="https://www.linkedin.com/in/c%C3%A9cile-palavit-5a765683/"
-            passHref
-          >
-            <a target="_blank">
-              <LinkedinOutlined
-                style={{ fontSize: "48px", color: "#0077b5" }}
-              />
-            </a>
-          </Link>
-        </div>
       </div>
+      <div className="flex justify-end mt-4 mr-24"></div>
     </section>
   );
 };
--- pages/mentions.jsx
+++ pages/mentions.jsx
@@ -9,10 +9,35 @@ const mentions = () => {
         <div className="m-6">
           <h2 className={style.title}>Mentions légales</h2>
           <p>
-            <b>Client :</b> tout professionnel ou personne physique capable au
-            sens des articles 1123 et suivants du Code civil, ou personne
-            morale, qui visite le Site objet des présentes conditions générales.
+            En vertu de l&apos;article 6 de la loi n° 2004-575 du 21 juin 2004
+            pour la confiance dans l&apos;économie numérique, il est précisé aux
+            utilisateurs du site internet{" "}
+            <a href="http://cecile-palavit-avocat.com">
+              cecile-palavit-avocat.com
+            </a>{" "}
+            l&apos;identité des différents intervenants dans le cadre de sa
+            réalisation et de son suivi:
+          </p>
+          <br />
+          <p>
+            <strong>Propriétaire</strong> : Entreprise Individuelle Cécile
+            Palavit - SIREN : N°822384517 - Numéro de TVA: FR79822384517 – 55
+            boulevard des brotteaux 69006 Lyon <br /> <br />
+            <strong>Responsable publication</strong> : Cécile Palavit –
+            (personne physique) contact@cecile-palavit-avocat.com
             <br />
+            <br />
+            <strong> Hebergeur : </strong> société OVH, SAS au capital de 10 069
+            020 €, RCS Lille Métropole 424 761 419 00045, Code APE 2620Z, N° TVA
+            : FR 22 424 761 419, Siège social : 2 rue Kellermann – 59100 Roubaix
+            – France. Téléphone : 09 72 10 10 07
+            <br />
+            <br />
+            <strong>Webmaster</strong> : Bastien Le Calvez –
+            bastien.lecalvez@gmail.com
+            <br />
+            <br />
+            <strong> Monogramme</strong> : Marie Adam - m-adam@outlook.fr
           </p>
         </div>
       </Layout>
